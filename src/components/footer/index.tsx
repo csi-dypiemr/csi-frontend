65f4cbf16abe66ffe769a12d3331a60fa1eb8b49
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-03-08 23:43:47 +0530

    align fix

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -59,7 +59,7 @@ export const Footer: React.FC = () => {
                 </Link>
               </p>
               <p>
-                <Link href={'https://csidypiemr.medium.com/'}>
+                <Link href={"https://csidypiemr.medium.com/"}>
                   <a target="_blank" className="text-white">
                     Blogs
                   </a>
@@ -108,21 +108,18 @@ export const Footer: React.FC = () => {
                     d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"
                   ></path>
                 </svg>
-               
-              
+
                 <span className="text-red-400 font-bold">
                   <Link href="mailto:csi.comp@dypiemr.ac.in">
                     <a> csi.comp@dypiemr.ac.in</a>
-                   
-                    </Link>
-                  </span>
-                    
+                  </Link>
+                </span>
               </p>
               <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
                 Find us on
               </h6>
 
-              <div className="flex justify-start lg:justify-start md:justify-start sm:justify-center">
+              <div className="flex justify-center lg:justify-start md:justify-start">
                 <a href="#!" className="mr-6 text-white">
                   <svg
                     aria-hidden="true"
@@ -237,5 +234,5 @@ export const Footer: React.FC = () => {
         </div>
       </footer>
     </div>
-  )
+  );
 }
